82c7b83def75b59c193e1935a7d15b202a32cfa0
hoangoliverblog <hoang682681@gmail.com>
2021-10-16 14:11:19 +0700

    comment seeder staff

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -20,6 +20,6 @@ class DatabaseSeeder extends Seeder
          $this->call(xeploaiSeeder::class);
          $this->call(hoadonSeeder::class);
          $this->call(chitiethoadonSeeder::class);
-         $this->call(StaffSeeder::class);
+        //  $this->call(StaffSeeder::class);
     }
 }
